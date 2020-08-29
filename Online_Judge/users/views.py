27afd050089d08eeb7fe24848dced36817c6b4ad
Roshan <roshan.mjain.cse19@itbhu.ac.in>
2020-08-29 21:58:20 +0530

    Resolved Login Issue

---
+++ Online_Judge/users/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import authenticate,login,logout
+
+
+# Create your views here.
+
+
+def index(request):
+    if not request.user.is_authenticated:
+        HttpResponseRedirect(reverse("login"))
+    return render(request,"users/user.html")
+
+def login_view(request):
+    if request.method=="POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user != None:
+            login(request, user)
+            print('hi')
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request,"users/login.html")
+    return render(request,"users/login.html") 
+
+def logout_view(request):
+    logout(request)
+    return render(request,"users/login.html")
+
+    
