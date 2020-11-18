745afcf4485cbdec46ddc411b5e9f210cf3e1ac6
ritik11g <goyalritik2114@gmail.com>
2020-11-18 15:19:13 +0530

    Frontend for Lab_works,Tutorial,Profile

--- Online_Judge/users/urls.py
+++ Online_Judge/users/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("home_page",views.home_page , name="home_page"),
+    path("lab_works",views.lab_works , name="lab_works"),
+    path("tutorial",views.tutorial , name="tutorial"),
+    path("profile",views.profile , name="profile"),
     #path("register",views.register,name="register")
 ]
--- Online_Judge/users/views.py
+++ Online_Judge/users/views.py
@@ -9,8 +9,8 @@ from .models import *
 
 def index(request):
     if not request.user.is_authenticated:
-        HttpResponseRedirect(reverse("login"))
-    return render(request,"users/user.html")
+        return HttpResponseRedirect(reverse("login"))
+    return render(request,"users/home_page.html")
 
 def login_view(request):
     if request.method=="POST":
@@ -22,11 +22,28 @@ def login_view(request):
             return HttpResponseRedirect(reverse("index"))
         else:
             return render(request,"users/login.html")
-    return render(request,"users/login.html") 
+    return render(request,"users/login.html")
 
 def logout_view(request):
     logout(request)
     return render(request,"users/login.html")
 
 def home_page(request):
-    return render(request, "users/home_page.html")
+    return render(request, "users/home_page.html",{
+        "page_name":"home_page"
+    })
+
+def lab_works(request):
+    return render(request, "users/lab_works.html",{
+        "page_name":"lab_works"
+    })
+
+def tutorial(request):
+    return render(request, "users/tutorial.html",{
+        "page_name":"tutorial"
+    })
+
+def profile(request):
+    return render(request, "users/profile.html",{
+        "page_name":"profile"
+    })
